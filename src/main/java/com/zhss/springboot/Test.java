3ea969349311fb982be7d5845f8b2512ccbd3787
developerkai <839203703@qq.com>
2018-07-14 17:29:38 +0800

    version

---
+++ src/main/java/com/zhss/springboot/Test.java
@@ -0,0 +1,11 @@
+package com.zhss.springboot;
+
+/**
+ * @author ceshi
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: ${todo}
+ * @date 2018/7/1417:28
+ */
+public class Test {
+}

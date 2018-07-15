072965456d798010e5e8cfe14544b3d26b08267f
developerkai <839203703@qq.com>
2018-07-15 17:08:40 +0800

    test

--- src/main/java/com/zhss/springboot/Test.java
+++ src/main/java/com/zhss/springboot/Test.java
@@ -11,6 +11,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("ss");
         System.out.println("zz");
+        System.out.println("test");
 
     }
 
